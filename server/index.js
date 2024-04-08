e10b13b619493f71769097f361cd3d6ae2b23389
shahul cb <theshahulofcl@gmail.com>
2024-04-08 14:47:39 +0530

    navbar,sidebar done. user data fetched from srever. api redux toolkit

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
+// data imports
+import User from "./models/User.js";
+
+import { dataUser } from "./data/index.js"
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -34,4 +39,6 @@ mongoose.connect(process.env.MONGO_URL, {
     // useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port : ${PORT}`))
+    /* ONLY ADD DATA ONE TIME */
+    // User.insertMany(dataUser)
 }).catch((error) => console.log(error.message))
