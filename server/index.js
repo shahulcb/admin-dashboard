2f00d13a7f50e5338a242f157d0e93fe25392f3f
shahul cb <theshahulofcl@gmail.com>
2024-04-17 00:48:10 +0530

    performance page

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 import {
   dataUser,
@@ -23,6 +24,7 @@ import {
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/index.js";
 
 // CONFIGURATION
@@ -57,5 +59,6 @@ mongoose
     // User.insertMany(dataUser)
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(error.message));
